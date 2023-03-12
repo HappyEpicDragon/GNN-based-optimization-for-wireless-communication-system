08644afbcd7aded3a8a7b4dc6757733df8c19130
LongXin <Xin.Long2000@outlook.com>
2023-03-12 23:18:02 +0800

    finish BCD, ready for debug

--- BCD.py
+++ BCD.py
@@ -29,15 +29,16 @@ class BCD:
         self.M = info.IRS_antennas
         self.N = info.BS_antennas
         self.K = info.user_num
-        self.alpha, self.beta, self.epsilon, self.W = self.BCD_init()
+        self.alpha, self.beta, self.epsilon, self.W, self.a, self.b = self.BCD_init()
 
-    @staticmethod
-    def BCD_init():
-        alpha = None
-        beta = None
-        epsilon = None
-        W = None
-        return alpha, beta, epsilon, W
+    def BCD_init(self):
+        alpha = np.ones([self.K, 1])
+        beta = np.ones([self.K, 1])
+        epsilon = np.ones([self.K, 1])
+        W = np.zeros([self.K, self.M])
+        a = None
+        b = None
+        return alpha, beta, epsilon, W, a, b
 
     def optimize(self, eps=1e-10):
         while True:
@@ -82,13 +83,94 @@ class BCD:
             self.beta[k] = beta_k
 
     def update_W(self):
-        pass
+        Lambda = 1e6
+        self.W_o(Lambda)
+        p = np.linalg.norm(self.W)
+        while p <= self.P_T:
+            Lambda /= 2
+            self.W_o(Lambda)
+            p = np.linalg.norm(self.W)
+
+    def W_o(self, Lambda):
+        alpha_hat = 1 + self.alpha
+        K = self.K
+        for k in range(K):
+            h_dk = self.h_d[k]
+            h_rk = self.h_r[k]
+            G_H = self.Hermit(self.G)
+            Theta = self.Theta
+            h_k = h_dk + np.matmul(np.matmul(G_H, Theta), h_rk)
+            alpha_hat_k = alpha_hat[k]
+            beta_k = self.beta[k]
+            inv = np.zeros([self.M, self.M])
+            for i in range(K):
+                h_di = self.h_d[i]
+                h_ri = self.h_r[i]
+                G_H = self.Hermit(self.G)
+                Theta = self.Theta
+                h_i = h_di + np.matmul(np.matmul(G_H, Theta), h_ri)
+                h_iH = self.Hermit(h_i)
+                beta_i = self.beta[i]
+                t = np.matmul(h_i, h_iH)
+                inv = inv + np.power(beta_i, 2) * t
+            inv = Lambda * np.eye(self.M) + inv
+            inv = np.linalg.inv(inv)
+            w_k = np.sqrt(alpha_hat_k) * beta_k * inv * h_k
+            self.W[k] = w_k
 
     def update_epsilon(self):
-        pass
+        self.a_b()
+        alpha_hat = self.alpha + 1
+        theta = np.diag(self.Theta)
+        theta_H = self.Hermit(theta)
+        K = self.K
+        for k in range(K):
+            alpha_hat_k = alpha_hat[k]
+            b_kk = self.b[k, k]
+            a_kk = self.a[k, k]
+            numerator = np.sqrt(alpha_hat_k) * (b_kk + np.matmul(theta_H, a_kk))
+            denominator = 0
+            for i in range(K):
+                b_ik = self.b[i, k]
+                a_ik = self.a[i, k]
+                t = b_ik + np.matmul(theta_H, a_ik)
+                t = np.linalg.norm(t)
+                t = np.power(t, 2)
+                denominator += t
+            denominator += self.sigma2
+            self.epsilon[k] = numerator / denominator
 
     def update_theta(self):
-        pass
+        K = self.K
+        U = np.zeros([self.N, self.N])
+        for k in range(K):
+            t = self.epsilon[k]
+            t = np.power(t, 2)
+            t1 = np.zeros([self.N, self.N])
+            for i in range(K):
+                a_ik = self.a[i, k]
+                a_ikH = self.Hermit(a_ik)
+                t1 = t1 + np.matmul(a_ik, a_ikH)
+            U = U + t * t1
+        v = np.zeros([1, self.N])
+        alpha_hat = self.alpha + 1
+        for k in range(K):
+            a_kk = self.a[k, k]
+            alpha_hat_k = alpha_hat[k]
+            epsilon_k = self.epsilon[k]
+            t1 = np.sqrt(alpha_hat_k) * epsilon_k.conjugate() * a_kk
+            t2 = np.zeros([1, self.N])
+            for i in range(K):
+                b_ik = self.b[i, k]
+                a_ik = self.a[i, k]
+                t2 = t2 + b_ik.conjugate() * a_ik
+            t2 = t2 * np.power(epsilon_k, 2)
+            v = v + t1 - t2
+        N = self.N
+        inv = np.eye(N) + U
+        inv = np.linalg.inv(inv)
+        theta = np.matmul(inv, v)
+        self.Theta = np.diag(theta)
 
     def f1(self):
         gamma = self.gamma()
@@ -138,6 +220,29 @@ class BCD:
             gamma_k = numerator / denominator
             gamma.append(gamma_k)
         return np.array(gamma)
+
+    def a_b(self):
+        a = []
+        b = []
+        K = self.K
+        for i in range(K):
+            w_i = self.W[i]
+            a_i = []
+            b_i = []
+            for k in range(K):
+                h_rk = self.h_r[k]
+                h_rkH = self.Hermit(h_rk)
+                a_ik = np.matmul(np.diag(h_rkH), self.G)
+                a_ik = np.matmul(a_ik, w_i)
+                h_dk = self.h_d[k]
+                h_dkH = self.Hermit(h_dk)
+                b_ik = np.matmul(h_dkH, w_i)
+                a_i.append(a_ik)
+                b_i.append(b_ik)
+            a.append(a_i)
+            b.append(b_i)
+        self.a = np.array(a)
+        self.b = np.array(b)
 
     @staticmethod
     def Hermit(matrix):
